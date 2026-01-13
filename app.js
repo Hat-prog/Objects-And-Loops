1ec6a801a82dd56c9d72134c4f1f2cbc50952ae2
Lukas <thokenm3@gmail.com>
2026-01-13 12:02:41 +0000

    Attacker and defender

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
-const Warrior = {
-    name: "Warrior",
-    hp: 100,
-    takeDamage: function (amount) {
-        this.hp -= amount;
-    }
-};
+const Characters = [
+  { name: "Warrior", hp: 100 },
+  { name: "Wizzard", hp: 77 },
+  { name: "Orc", hp: 150 }
+];
 
-Warrior.takeDamage(10);
-console.log(Warrior.hp);
+function takeDamage(character, amount) {
+  character.hp -= amount;
+}
 
-const Wizzard = {
-    name: "Wizzard",
-    hp: 70,
-    takeDamage: function (amount) {
-        this.hp -= amount;
-    }
-};
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
 
-Wizzard.takeDamage(15);
-console.log(Wizzard.hp);
+function battleTurn(characters) {
+  const attackerIndex = getRandomIndex(characters);
+  let defenderIndex = getRandomIndex(characters);
 
-const Orc = {
-    name: "Orc",
-    hp: 200,
-    takeDamage: function (amount) {
-        this.hp -= amount;
-    }
-};
+  while (attackerIndex === defenderIndex) {
+    defenderIndex = getRandomIndex(characters);
+  }
 
-Orc.takeDamage(77);
-console.log(Orc.hp);
+  const attacker = characters[attackerIndex];
+  const defender = characters[defenderIndex];
+
+  const damage = Math.floor(Math.random() * 20) + 1;
+
+  takeDamage(defender, damage);
+
+  console.log(
+    attacker.name + " attacks " + defender.name + " and deals " + damage + " damage"
+  );
+}
+
+battleTurn(Characters);
